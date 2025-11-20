d46b599a6278bcd9b51120db77888e005eba83ff
Priyansh Balyan <priyanshbalyan@gmail.com>
2025-11-20 21:58:29 +0900

    Refactor fs.py: Fix lint issues, rename functions to snake_case, improve file handling

--- fs.py
+++ fs.py
@@ -3,7 +3,6 @@ import json
 import sys
 import os
 import io
-import re
 from time import sleep
 
 # USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'
@@ -21,7 +20,7 @@ CHUNK_SIZE = 8 * 1000 * 1000  # Discord 8MB file limit
 
 
 # Get size unit text from number of bytes
-def getSizeFormat(size):
+def get_size_format(size):
     unit = ["TB", "GB", "MB", "KB", "B"]
     while size / 1024 > 1:
         size /= 1024.0
@@ -30,7 +29,7 @@ def getSizeFormat(size):
     return "%.*f %s" % (2, size, unit[-1])
 
 
-def loadFileIndex():
+def load_file_index():
     response = requests.get(f"{BASE_URL}{CHANNEL_ID}/messages?limit=1", headers=headers)
     if response.status_code != 200:
         print(
@@ -52,20 +51,17 @@ def loadFileIndex():
         print("No index file found")
         return
 
-    f = open(INDEX_FILE, "w")
-    response = requests.get(url)
-
-    f.write(response.text)
-    f.close()
+    with open(INDEX_FILE, "w") as f:
+        response = requests.get(url)
+        f.write(response.text)
 
     return last_message["id"]
 
 
-def getFileIndex():
+def get_file_index():
     try:
-        f = open(INDEX_FILE, "r")
-        data = f.read()
-        f.close()
+        with open(INDEX_FILE, "r") as f:
+            data = f.read()
         return json.loads(data)
     except FileNotFoundError:
         return dict()
@@ -100,7 +96,7 @@ def decode(encoded):
     return decoded
 
 
-def printTableHeader():
+def print_table_header():
     terminal_size = os.get_terminal_size()
 
     max_width = min(120, terminal_size[0]) - 22
@@ -111,40 +107,40 @@ def printTableHeader():
     return formatting, max_width
 
 
-def printTableRow(number, filename, size, formatting, max_width):
+def print_table_row(number, filename, size, formatting, max_width):
     if len(filename) > max_width:
         line = filename[max_width:]
         print(
-            formatting % (filename[:max_width], getSizeFormat(size), "#" + str(number))
+            formatting % (filename[:max_width], get_size_format(size), "#" + str(number))
         )
         while line:
             print(line[:max_width])
             line = line[max_width:]
     else:
-        print(formatting % (filename, getSizeFormat(size), "#" + str(number)))
+        print(formatting % (filename, get_size_format(size), "#" + str(number)))
 
 
-def listFiles(args):
-    loadFileIndex()
-    file_index = getFileIndex()
+def list_files(args):
+    load_file_index()
+    file_index = get_file_index()
 
-    formatting, maxwidth = printTableHeader()
+    formatting, maxwidth = print_table_header()
     total_size = 0
 
     for i, values in enumerate(file_index.values()):
         filename = decode(values["filename"])
         total_size += values["size"]
-        printTableRow(i + 1, filename, values["size"], formatting, maxwidth)
+        print_table_row(i + 1, filename, values["size"], formatting, maxwidth)
 
     terminal_size = os.get_terminal_size()
     max_width = min(120, terminal_size[0])
     print("-" * max_width)
-    print(f"Total storage used: {getSizeFormat(total_size)}")
+    print(f"Total storage used: {get_size_format(total_size)}")
 
 
-def findFile(args):
-    loadFileIndex()
-    file_index = getFileIndex()
+def find_file(args):
+    load_file_index()
+    file_index = get_file_index()
 
     results = []
     for i, values in enumerate(file_index.values()):
@@ -153,25 +149,24 @@ def findFile(args):
             results.append((i + 1, filename, values["size"]))
 
     if len(results) > 0:
-        formatting, maxwidth = printTableHeader()
+        formatting, maxwidth = print_table_header()
         for result_num, result_filename, result_size in results:
-            printTableRow(
+            print_table_row(
                 result_num, result_filename, result_size, formatting, maxwidth
             )
     else:
         print("No matching files found in the server.")
 
 
-def getTotalChunks(size):
+def get_total_chunks(size):
     if size / CHUNK_SIZE > 1:
         return size // CHUNK_SIZE + 1
     return 1
 
 
-def updateFileIndex(index_id, file_index):
-    f = open(INDEX_FILE, "w")
-    f.write(json.dumps(file_index))
-    f.close()
+def update_file_index(index_id, file_index):
+    with open(INDEX_FILE, "w") as f:
+        f.write(json.dumps(file_index))
 
     files = [["", [INDEX_FILE, open(INDEX_FILE, "rb")]]]
 
@@ -198,7 +193,7 @@ def updateFileIndex(index_id, file_index):
     print("Done.")
 
 
-def showProgressBar(iteration, total):
+def show_progress_bar(iteration, total):
     decimals = 2
     length = min(120, os.get_terminal_size()[0]) - 40
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
@@ -209,68 +204,68 @@ def showProgressBar(iteration, total):
         print()
 
 
-def uploadFile(args):
-    message_id = loadFileIndex()
+def upload_file(args):
+    message_id = load_file_index()
     try:
         f = open(args[0], "rb")
     except FileNotFoundError as err:
         print(err)
         sys.exit()
 
-    file_index = getFileIndex()
+    with f:
+        file_index = get_file_index()
 
-    size = os.path.getsize(args[0])
-    filename = os.path.basename(args[0])
-    total_chunks = getTotalChunks(size)
+        size = os.path.getsize(args[0])
+        filename = os.path.basename(args[0])
+        total_chunks = get_total_chunks(size)
 
-    if encode(filename) in file_index:
-        print("File already uploaded.")
-        sys.exit()
-
-    print("File Name: ", filename)
-    print("File Size: ", getSizeFormat(size))
-    print("Chunks to be created: ", total_chunks)
-    print("Uploading...")
-
-    urls = []
-    for i in range(total_chunks):
-        showProgressBar(i + 1, total_chunks)
-        chunk = io.BytesIO(f.read(CHUNK_SIZE))  # Read file in 8MB chunks
-        files = [["", [encode(filename) + "." + str(i), chunk]]]
-
-        response = requests.post(
-            f"{BASE_URL}{CHANNEL_ID}/messages", headers=headers, files=files
-        )
-        if response.status_code != 200:
-            print("Error encountered while uploading file:", response.text)
+        if encode(filename) in file_index:
+            print("File already uploaded.")
             sys.exit()
 
-        message = response.json()
-        urls.append(
-            [message["id"], message["attachments"][0]["id"]]
-        )  # message_id, attachment_id pair
+        print("File Name: ", filename)
+        print("File Size: ", get_size_format(size))
+        print("Chunks to be created: ", total_chunks)
+        print("Uploading...")
 
-    print("File uploaded")
+        urls = []
+        for i in range(total_chunks):
+            show_progress_bar(i + 1, total_chunks)
+            chunk = io.BytesIO(f.read(CHUNK_SIZE))  # Read file in 8MB chunks
+            files = [["", [encode(filename) + "." + str(i), chunk]]]
 
-    file_index[encode(filename)] = {
-        "filename": encode(filename),
-        "size": size,
-        "urls": urls,
-    }
-    updateFileIndex(message_id, file_index)
-    f.close()
+            response = requests.post(
+                f"{BASE_URL}{CHANNEL_ID}/messages", headers=headers, files=files
+            )
+            if response.status_code != 200:
+                print("Error encountered while uploading file:", response.text)
+                sys.exit()
+
+            message = response.json()
+            urls.append(
+                [message["id"], message["attachments"][0]["id"]]
+            )  # message_id, attachment_id pair
+
+        print("File uploaded")
+
+        file_index[encode(filename)] = {
+            "filename": encode(filename),
+            "size": size,
+            "urls": urls,
+        }
+        update_file_index(message_id, file_index)
 
 
-def downloadFile(args):
+def download_file(args):
     indices = []
     for arg in args:
         indices.append(int(arg[1:]) if arg[0] == "#" else int(arg) - 1)
 
-    loadFileIndex()
-    file_index = getFileIndex()
+    load_file_index()
+    file_index = get_file_index()
     filelist = list(file_index.items())
 
-    file_regex = r"[&+()\[\]@–',]"
+    # file_regex = r"[&+()\[\]@–',]" # Unused
 
     for index in indices:
         if index >= len(filelist):
@@ -282,37 +277,36 @@ def downloadFile(args):
         og_name, file = filelist[index]
         filename = decode(file["filename"])
         os.makedirs(os.path.dirname(f"downloads/{filename}"), exist_ok=True)
-        f = open("downloads/" + filename, "wb")
-        for i, values in enumerate(file["urls"]):
-            # TODO: Remove attachment_id from index file
-            message_id, attachment_id = values
+        
+        with open("downloads/" + filename, "wb") as f:
+            for i, values in enumerate(file["urls"]):
+                # TODO: Remove attachment_id from index file
+                message_id, attachment_id = values
 
-            response = requests.get(f"{BASE_URL}{CHANNEL_ID}/messages/{message_id}", headers=headers)
-            if response.status_code != 200:
-                print("An error occurred while loading messages: ", response.status_code, response.text)
-                sys.exit()
+                response = requests.get(f"{BASE_URL}{CHANNEL_ID}/messages/{message_id}", headers=headers)
+                if response.status_code != 200:
+                    print("An error occurred while loading messages: ", response.status_code, response.text)
+                    sys.exit()
 
-            attachments = response.json()['attachments']
-            download_url = attachments[0]['url']
+                attachments = response.json()['attachments']
+                download_url = attachments[0]['url']
 
-            cdnResponse = requests.get(download_url)  # download from url with identification params
-            if cdnResponse.status_code != 200:
-                print("An error occurred while downloading the file:", cdnResponse.status_code, cdnResponse.text)
-                sys.exit()
+                cdnResponse = requests.get(download_url)  # download from url with identification params
+                if cdnResponse.status_code != 200:
+                    print("An error occurred while downloading the file:", cdnResponse.status_code, cdnResponse.text)
+                    sys.exit()
 
-            showProgressBar(i + 1, len(file["urls"]))
-            f.write(cdnResponse.content)
-
-        f.close()
+                show_progress_bar(i + 1, len(file["urls"]))
+                f.write(cdnResponse.content)
 
     print("Download complete.")
 
 
-def deleteFile(args):
+def delete_file(args):
     index = (int(args[0][1:]) if args[0][0] == "#" else int(args[0])) - 1
-    index_message_id = loadFileIndex()
+    index_message_id = load_file_index()
 
-    file_index = getFileIndex()
+    file_index = get_file_index()
     file_list = list(file_index.values())
     if index >= len(file_list):
         print("Invalid ID provided")
@@ -334,17 +328,17 @@ def deleteFile(args):
             )
             sys.exit()
 
-        showProgressBar(i + 1, len(message_ids))
+        show_progress_bar(i + 1, len(message_ids))
         sleep(1)
 
     del file_index[file["filename"]]
-    updateFileIndex(index_message_id, file_index)
+    update_file_index(index_message_id, file_index)
     print(f"Deleted {decode(file['filename'])}.")
 
 
-def renameFile(args):
-    message_id = loadFileIndex()
-    file_index = getFileIndex()
+def rename_file(args):
+    message_id = load_file_index()
+    file_index = get_file_index()
 
     index = (int(args[0][1:]) if args[0][0] == "#" else int(args[0])) - 1
     filelist = list(file_index.values())
@@ -358,11 +352,11 @@ def renameFile(args):
     urls = file["urls"]
     print("File details:")
     print("Name: ", decode(file["filename"]))
-    print("Size: ", getSizeFormat(file["size"]))
+    print("Size: ", get_size_format(file["size"]))
     rename_text = input("Enter new file name: ")
     file["filename"] = encode(rename_text)
     file_index[filename] = {"filename": encode(rename_text), "size": size, "urls": urls}
-    updateFileIndex(message_id, file_index)
+    update_file_index(message_id, file_index)
     print("File renamed")
 
 
@@ -370,42 +364,42 @@ def init():
     commands = [
         {
             "alias": ["-l", "-list"],
-            "function": listFiles,
+            "function": list_files,
             "minArgs": 0,
             "syntax": "-l",
             "desc": "Lists all the file information that has been uploaded to the server.",
         },
         {
             "alias": ["-u", "-upload"],
-            "function": uploadFile,
+            "function": upload_file,
             "minArgs": 1,
             "syntax": "-u path/to/file",
             "desc": "Uploads a file to the server. The full file directory is taken in for the argument.",
         },
         {
             "alias": ["-d", "-download"],
-            "function": downloadFile,
+            "function": download_file,
             "minArgs": 1,
             "syntax": "-d #ID",
             "desc": "Downloads a file from the server. An #ID is taken in as the file identifier. Provide multiple ids separated by space to download multiple files",
         },
         {
             "alias": ["-del", "-delete"],
-            "function": deleteFile,
+            "function": delete_file,
             "minArgs": 1,
             "syntax": "-del #ID",
             "desc": "Deletes a file from the server. An #ID is taken in as the file identifier",
         },
         {
             "alias": ["-f", "-find"],
-            "function": findFile,
+            "function": find_file,
             "minArgs": 1,
             "syntax": "-f text_to_search",
             "desc": "Finds files with matching text",
         },
         {
             "alias": ["-r", "-rename"],
-            "function": renameFile,
+            "function": rename_file,
             "minArgs": 1,
             "syntax": "-r #ID",
             "desc": "Rename a file in the server. An #ID is taken in as the file identifier",
@@ -415,16 +409,14 @@ def init():
     global TOKEN, CHANNEL_ID, headers, CDN_BASE_URL
 
     try:
-        f = open(".env", "r")
-        TOKEN = f.readline().split("=")[1].strip()
-        CHANNEL_ID = f.readline().split("=")[1].strip()
-        f.close()
-    except FileNotFoundError or IndexError:
+        with open(".env", "r") as f:
+            TOKEN = f.readline().split("=")[1].strip()
+            CHANNEL_ID = f.readline().split("=")[1].strip()
+    except (FileNotFoundError, IndexError):
         TOKEN = input("Enter bot token to be used: ")
         CHANNEL_ID = input("Enter discord channel id to be used to store files: ")
-        f = open(".env", "w")
-        f.write(f"TOKEN={TOKEN}\nCHANNEL_ID={CHANNEL_ID}")
-        f.close()
+        with open(".env", "w") as f:
+            f.write(f"TOKEN={TOKEN}\nCHANNEL_ID={CHANNEL_ID}")
 
     headers = {"Authorization": f"Bot {TOKEN}"}
     CDN_BASE_URL = f"https://cdn.discordapp.com/attachments/{CHANNEL_ID}/"
